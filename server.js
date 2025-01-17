5f25da2aaa9058495d75f36d74d77599dc8213fc
Filipe Braga <filipebragabrasil@hotmail.com>
2025-01-17 13:25:23 -0300

    feat: created server and listener

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Wow!');
+})
+
+server.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+});
