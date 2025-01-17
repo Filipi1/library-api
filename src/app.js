69c20b8e918e39f27a49a4e265af957976d99744
Filipe Braga <filipebragabrasil@hotmail.com>
2025-01-17 14:32:23 -0300

    feat: added, get by id, put and delete methods to books

--- src/app.js
+++ src/app.js
@@ -13,9 +13,40 @@ app.get("/livros", (req, res) => {
     res.status(200).json(books);
 })
 
+app.get("/livros/:id", (req, res) => {
+    const bookIndex = books.findIndex(book => book.id === parseInt(req.params.id));
+    if (bookIndex < 0) {
+        res.status(404).send("Book not found");
+        return
+    }
+    res.status(201).json(books[bookIndex]);
+})
+
 app.post("/livros", (req, res) => {
     books.push(req.body);
     res.status(201).json(books);
 })
+
+app.put("/livros/:id", (req, res) => {
+    const bookIndex = books.findIndex(book => book.id === parseInt(req.params.id));
+    if (bookIndex < 0) {
+        res.status(404).send("Book not found");
+        return
+    }
+
+    books[bookIndex].title = req.body.title;
+    res.status(201).json(books);
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const bookIndex = books.findIndex(book => book.id === parseInt(req.params.id));
+    if (bookIndex < 0) {
+        res.status(404).send("Book not found");
+        return
+    }
+    books.splice(bookIndex, 1);
+    res.status(201).json(books);
+})
+
 
 export default app
