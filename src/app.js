cb0f9be7703912c4b8f308a68ac0a02a49bdec92
Filipe Braga <filipebragabrasil@hotmail.com>
2025-01-17 14:16:18 -0300

    feat: added post to create books

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
 const books = [{ id: 1, "title": "Book #1"}, {id: 2, "title": "Book #2"}];
 
 app.get("/", (req, res) => {
@@ -9,6 +11,11 @@ app.get("/", (req, res) => {
 
 app.get("/livros", (req, res) => {
     res.status(200).json(books);
+})
+
+app.post("/livros", (req, res) => {
+    books.push(req.body);
+    res.status(201).json(books);
 })
 
 export default app
