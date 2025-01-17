6f30bde38afb9927c1e0a4d66d7121404ebd3c29
Filipe Braga <filipebragabrasil@hotmail.com>
2025-01-17 13:32:38 -0300

    feat: remove node_modules

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+const routes = {
+    '/': "Teste 1",
+    '/livros': "Livros!",
+    '/autores': "Autores!"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Wow!');
+    res.end(routes[req.url]);
 })
 
 server.listen(port, () => {
